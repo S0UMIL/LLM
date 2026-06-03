30fb1a49d5e1cef50e72c8b24bb3b0e6d0c6c038
Soumil Tiwary <soumil.tiwary@gmail.com>
2026-06-03 14:25:30 +0530

    Add basic framework for Langchain with OllamaLLM
    Implement basic Streamlit framework with OllamaLLM integration.

---
+++ basic_framework_w_llama.py
@@ -0,0 +1,16 @@
+#importing all the stuff.
+import os
+from langchain_ollama import OllamaLLM
+import streamlit as st
+
+##Streamlit framework
+
+st.title('Langchain Practice using OpenAI')
+input_text=st.text_input("search the topic you want")
+
+##OPENAI LLMS
+llm=OllamaLLM(model="llama3")
+
+if input_text:
+    st.write(llm.invoke(input_text))
+  #basic framework of importing llama (when locally installed on ur device and working on top of it)
